4203d8891b3d3ee55814aaeaa102ad00ede1f41f
mistvle <mistle.roblox@gmail.com>
2026-06-04 23:18:42 -0700

    A

--- buttons/pricing.js
+++ buttons/pricing.js
@@ -3,18 +3,33 @@ module.exports = {
 
     async execute (interaction) {
         return interaction.reply({
-  "flags": 32832,
+  "username": "Klopps Commissions",
+  "avatar_url": "https://cdn.discordapp.com/emojis/1508232616623738890.webp?size=240",
+  "flags": 32768,
   "components": [
     {
       "type": 17,
       "components": [
         {
           "type": 10,
-          "content": "- Simple Embeds: 60R$ each\n- Advanced Embeds: 80R$ - 100R$ each\n-# Pricing depends on your order complexity, budget, and quantity. More information will be provided to you upon opening a ticket."
+          "content": "# <:unknown:1511491360744669357> **Pricing**\nAt <:unknown:1508232616623738890> **Klopps Commissions** believe in reasonable pricing, while ensuring we produce quality products. Pricing is done based on the complexity of what you would like, but we always try and keep it as affordable as possible.\n"
+        }
+      ]
+    },
+    {
+      "type": 17,
+      "components": [
+        {
+          "type": 10,
+          "content": "# <:unknown:1408923523581476934> Accepted Payments\nCurrently we are only accepting <:unknown:1408923523581476934> **Robux.** As we expand in the future, we will look to switching to a wider variety of payment types, which are tax free as Roblox's 30% tax doesn't help. "
         }
       ]
     }
-  ]
+  ],
+  "author": {
+    "icon_url": "https://cdn.discordapp.com/emojis/1508232616623738890.webp?size=240",
+    "name": "Klopps Commissions"
+  }
 })
     }
 }
